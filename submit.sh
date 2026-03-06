d3181d8484e600241635e9204f04cdf9a59f321f
Edward He <edwardhe@Edwards-MacBook-Pro.local>
2026-03-06 12:59:57 -0500

    change to submit.sh

--- submit.sh
+++ submit.sh
@@ -10,3 +10,8 @@ mkdir -p /home/uottawa.o.univ/ehe058/CSI5340_Assignment2-main/out
 cd /home/uottawa.o.univ/ehe058/CSI5340_Assignment2-main
 source /home/uottawa.o.univ/ehe058/CSI5340_Assignment2-main/myenv/local/bin/activate
 python main.py
+
+# Cleanup (optional)
+find . -type f -name '._*' -delete 2>/dev/null                        # Remove macOS metadata files
+find . -type d -name '__pycache__' -exec rm -rf {} + 2>/dev/null      # Remove Python cache directories
+find . -type d -name '.ipynb_checkpoints' -exec rm -rf {} + 2>/dev/null  # Remove Jupyter checkpoint directories
